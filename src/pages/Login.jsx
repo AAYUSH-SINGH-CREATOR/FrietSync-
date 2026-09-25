9b14c1d254b5f88dc537d69af668213bb1b0eea7
Aayush Singh Chauhan <1211aayushh@gmail.com>
2026-09-26 01:27:04 +0530

    feat: add signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,11 @@
+import { Link } from "react-router-dom"
+export default function Login(){
+
+  return(
+    <div>
+         <p className="text-center text-4xl-red font-bold">hello, this is login page</p>
+         <Link to="/signup">back to signup</Link>
+    </div>
+  )
+}
 
